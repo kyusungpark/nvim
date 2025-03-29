7416ee604cb3a64035d302115409eaddfd8818e0
Kyu Sung Park <qkrrbtjd90@gmail.com>
2025-03-28 21:00:52 -0400

    theme

--- custom/chadrc.lua
+++ custom/chadrc.lua
@@ -1,7 +1,7 @@
 ---@type ChadrcConfig
 local M = {}
 
-M.ui = { theme = 'github_dark' }
+M.ui = { theme = 'doomchad' }
 M.plugins = "custom.plugins"
 M.mappings = require "custom.configs.mappings"
 
